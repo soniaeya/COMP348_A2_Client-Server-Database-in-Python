9fd26cac2c63d0607d0a714bafea0a12d175320b
Sonia Li <96924335+soniaeya@users.noreply.github.com>
2022-11-15 12:01:01 -0500

    Add files via upload

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 # echo-client.py
 
 import socket
+import sys
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 9999  # The port used by the server
@@ -10,23 +11,29 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     exit_bool = False
     while not exit_bool:
-        select = int(input("""
-        Python DB Menu
-        1. Find customer
-        2. Add customer
-        3. Delete customer
-        4. Update customer age
-        5. Update customer address
-        6. Update customer phone
-        7. Print report
-        8. Exit
-        Select: """))
+        select = input(
+            """Python DB Menu
+1. Find customer
+2. Add customer
+3. Delete customer
+4. Update customer age
+5. Update customer address
+6. Update customer phone
+7. Print report
+8. Exit
+Select: """)
         send = ""
+
+        if not select.isdigit():
+            print("Please enter an integer!")
+            continue
+        select = int(select)
+
 
         if select == 1:
             name = input("Please enter a name: ")  # Get user input
 
-            send = str(select)+"|"+name
+            send = str(select) + "&" + name
             s.send(bytes(send, 'utf-8'))
             response = s.recv(1024)
             print(response.decode("utf-8"))
@@ -45,7 +52,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             new_customer_phone_num_str = input("Enter the phone number of the new customer: ")
             new_customer_str += new_customer_phone_num_str
 
-            send = str(select) + "&"+new_customer_str
+            send = str(select) + "&" + new_customer_str
             s.send(bytes(send, 'utf-8'))
             response = s.recv(1024)
             print(response.decode("utf-8"))
@@ -69,19 +76,36 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print(response.decode("utf-8"))
 
         elif select == 5:
-            print()
+            name_5 = input("Please enter the name of the person whose information you want to update from the "
+                           "database: ")
+            new_address = input("Please enter the address you wish to change to: ")
+
+            send = str(select) + "&" + name_5 + "&" + new_address
+            s.send(bytes(send, 'utf-8'))
+            response = s.recv(1024)
+            print(response.decode("utf-8"))
         elif select == 6:
-            print()
+            name_6 = input("Please enter the name of the person whose information you want to update from the "
+                           "database: ")
+            new_phone_num = input("Please enter the phone number you wish to change to: ")
+
+            send = str(select) + "&" + name_6 + "&" + new_phone_num
+            s.send(bytes(send, 'utf-8'))
+            response = s.recv(1024)
+            print(response.decode("utf-8"))
+
         elif select == 7:
-            print()
+            send = str(select)
+            s.send(bytes(send, 'utf-8'))
+            response = s.recv(1024)
+            print(response.decode("utf-8"))
+
         elif select == 8:
-            print()
+            print("Goodbye!")
+            exit_bool = True
+            s.close()
+
+        else:
+            print("Please enter a valid input!")
 
 
-
-
-
-
-
-
-print(f"Received {response!r}")
--- server.py
+++ server.py
@@ -15,7 +15,8 @@ counter = 0
 for i in file:
     # i: raw string from file
     # i = "".join(i.split())  # Deletes all whitespace, tabs, and spaces
-    i = i.replace("\t", "")
+
+    #i = i.replace("\t", "")
     i = i.replace("\n", "")
 
     # i: string with | as delimiter
@@ -30,16 +31,23 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print(f"Connected by {addr}")
+
         while True:
             request = conn.recv(1024)
             request = request.decode("utf-8")
-            request_list = request.split("&")
 
-            select = int(request_list[0])
-            input_str = str(request_list[1])
-            print(database)
+            if request == "":
+                select = 0
+            else:
+                request_list = request.split("&")
+                select = int(request_list[0])
+
+
+
+            break_out_flag = False
 
             if select == 1:
+                input_str = str(request_list[1])
 
                 name_bool = False
                 for i in database:
@@ -49,14 +57,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         for j in i:
                             customer_info += j + "|"
 
-                        customer_info = customer_info[:len(customer_info) - 1]
+                        customer_info = "Server Response: " + str(customer_info[:len(customer_info) - 1]) + "\n"
                         conn.send(bytes(customer_info, 'utf-8'))
 
                 if not name_bool:
-                    name_exist = bytes("The name is not found in database", 'utf-8')
+                    name_exist = bytes("Server Response: " + input_str + " not found in database", 'utf-8')
                     conn.send(name_exist)
-            elif select == 2:
+                not_stopping = False
 
+            elif select == 2:
+                input_str = str(request_list[1])
                 new_customer_info = input_str.split("|")
                 name_bool = False
                 for i in database:
@@ -64,17 +74,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         name_bool = True
 
                 if name_bool:
-                    response = "Customer already in database"
-                    conn.send(bytes(response, 'utf-8'))
+                    response_2_bad = "Customer already exists\n"
+                    conn.send(bytes(response_2_bad, 'utf-8'))
 
                 else:
-                    database += tuple(new_customer_info)
+                    database.append(tuple(new_customer_info))
                     file_append = open("data.txt", "a")
-                    file_append.write(input_str + "\n")
+                    file_append.write("\n" + input_str)
                     file_append.close()
-                    response = "You have successfully entered information into the database!"
-                    conn.send(bytes(response, 'utf-8'))
+                    response_2_good = "You have successfully entered information into the database!"
+                    conn.send(bytes(response_2_good, 'utf-8'))
+
             elif select == 3:
+                input_str = str(request_list[1])
                 for i in database:
                     if input_str == i[0]:
                         database.remove(i)
@@ -92,20 +104,123 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                             file_rewrite.truncate
                             file_rewrite.write(replacement_file_text)
 
-                        response = "You have successfully removed " + input_str
-                        conn.send(bytes(response, 'utf-8'))
+                        response_3_good = "You have successfully removed " + input_str
+                        conn.send(bytes(response_3_good, 'utf-8'))
+                        break_out_flag = True
+                        break
+                    if break_out_flag:
+                        break
 
-                response = "This person is not in the database "
-                conn.send(bytes(response, 'utf-8'))
+                if counter == len(database) - 1:
+                    response = "Customer does not exist"
+                    conn.send(bytes(response, 'utf-8'))
 
             elif select == 4:
+                input_str = str(request_list[1])
                 age = request_list[2]
                 counter = -1
                 for i in database:
-                    counter += counter
+                    counter += 1
                     if input_str == str(i[0]):
                         info_as_list = list(database[counter])
                         info_as_list[1] = age
+                        info_as_tuple = tuple(info_as_list)
+
+                        database[counter] = info_as_tuple
+                        new_info_str = ""
+
+                        for j in info_as_tuple:
+                            new_info_str += j + "|"
+
+                        new_info_str = new_info_str[:len(new_info_str) - 1]
+                        #new_info_str += "\n"
+
+                        replacement_file_text = ""
+
+                        with open("data.txt", "r") as input:
+                            # iterate all lines from file
+                            for line in input:
+                                # if substring contain in a line then don't write it
+
+                                if input_str in line.strip("\n"):
+                                    replacement_file_text += new_info_str
+                                    continue
+                                replacement_file_text += line
+                            input.close()
+                        with open("data.txt", "w") as file_rewrite:
+                            file_rewrite.truncate
+                            file_rewrite.write(replacement_file_text)
+
+                        response = "The database has been updated!"
+                        conn.send(bytes(response, 'utf-8'))
+                        break_out_flag = True
+                        break
+                    if break_out_flag:
+                        break
+
+                if counter == len(database) - 1:
+                    response = "Customer not found"
+                    conn.send(bytes(response, 'utf-8'))
+
+            elif select == 5:
+                input_str = str(request_list[1])
+
+                new_address = request_list[2]
+                counter = -1
+                for i in database:
+                    counter += 1
+                    if input_str == str(i[0]):
+                        info_as_list = list(database[counter])
+                        info_as_list[2] = new_address
+                        info_as_tuple = tuple(info_as_list)
+
+                        database[counter] = info_as_tuple
+                        new_info_str = ""
+
+                        for j in info_as_tuple:
+                            new_info_str += j + "|"
+
+                        new_info_str = new_info_str[:len(new_info_str) - 1]
+                        #if counter != len(database)-1:
+                         #   new_info_str += "\n"
+
+                        replacement_file_text = ""
+
+                        with open("data.txt", "r") as input:
+                            # iterate all lines from file
+                            for line in input:
+                                # if substring contained in a line then don't write it
+
+                                if input_str in line.strip("\n"):
+                                    replacement_file_text += new_info_str
+                                    continue
+                                replacement_file_text += line
+                            input.close()
+                        with open("data.txt", "w") as file_rewrite:
+                            file_rewrite.truncate
+                            file_rewrite.write(replacement_file_text)
+
+                        response = "The database has been updated!"
+                        conn.send(bytes(response, 'utf-8'))
+                        break_out_flag = True
+                        break
+                    if break_out_flag:
+                        break
+
+                if counter == len(database) - 1:
+                    response = "Customer not found"
+                    conn.send(bytes(response, 'utf-8'))
+
+            elif select == 6:
+                input_str = str(request_list[1])
+                new_phone_num = str(request_list[2])
+                counter = -1
+
+                for i in database:
+                    counter += 1
+                    if input_str == str(i[0]):
+                        info_as_list = list(database[counter])
+                        info_as_list[3] = new_phone_num
                         info_as_tuple = tuple(info_as_list)
 
                         database[counter] = info_as_tuple
@@ -131,18 +246,30 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         with open("data.txt", "w") as file_rewrite:
                             file_rewrite.truncate
                             file_rewrite.write(replacement_file_text)
-
+                            print("Option 6")
+                            print(database)
                         response = "The database has been updated!"
                         conn.send(bytes(response, 'utf-8'))
+                        break_out_flag = True
+                        break
+                    if break_out_flag:
+                        break
 
-                response = "This person is not in the database "
-                conn.send(bytes(response, 'utf-8'))
+                if counter == len(database)-1:
+                    response = "Customer not found"
+                    conn.send(bytes(response, 'utf-8'))
 
-            elif select == 5:
-                print()
-            elif select == 6:
-                print()
             elif select == 7:
-                print()
+
+                sort_database = sorted(database)
+                txt = "** Python DB contents **+\n"
+                for i in sort_database:
+                    for j in i:
+                        txt += j + "|"
+                    txt = txt[:len(txt) - 1]
+                    txt += "\n"
+
+                conn.send(bytes(txt, 'utf-8'))
+
             elif select == 8:
                 print()
